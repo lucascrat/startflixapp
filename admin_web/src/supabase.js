8c97c7ad402c75c84021bbf131289cf9b83a4ee4
HL Desenvolvedor <hldesenvolvedor@gmail.com>
2026-05-16 12:06:36 -0300

    Update admin_web to point to Coolify PostgREST

--- admin_web/src/supabase.js
+++ admin_web/src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Usando exatamente as mesmas chaves que o App Flutter usa
-const supabaseUrl = 'https://qyagfghcnzenvbhbtsvd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5YWdmZ2hjbnplbnZiaGJ0c3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3NDU2NjksImV4cCI6MjA4MzMyMTY2OX0.k_cVE7tLn23NIuuMJlCdWw97F_ZkPpz7SS7d-MleJVc'
+const supabaseUrl = 'http://g118gaedeyy792j9l0t7hbzc.84.247.138.242.sslip.io'
+const supabaseAnonKey = 'empty'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     db: {
