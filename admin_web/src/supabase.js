6cb85987850cae8fd7aa101578af7f9ffa315295
HL Desenvolvedor <hldesenvolvedor@gmail.com>
2026-05-16 12:55:43 -0300

    Switch to custom API domain https://apistartflixpainel.appbr.pro

--- admin_web/src/supabase.js
+++ admin_web/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'http://g118gaedeyy792j9l0t7hbzc.84.247.138.242.sslip.io'
+const supabaseUrl = 'https://apistartflixpainel.appbr.pro'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc4OTQ1MDQ4LCJleHAiOjIwOTQzMDUwNDh9.MP2-5TXurfkLspwA_3vft9g6nIY8sUHOBaqxPfkaKBg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -9,7 +9,7 @@ class AppColors {
 
 class AppConstants {
   // TODO: Substitua pelas suas credenciais do Supabase
-  static const String supabaseUrl = 'http://g118gaedeyy792j9l0t7hbzc.84.247.138.242.sslip.io';
+  static const String supabaseUrl = 'https://apistartflixpainel.appbr.pro';
   static const String supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc4OTQ1MDQ4LCJleHAiOjIwOTQzMDUwNDh9.MP2-5TXurfkLspwA_3vft9g6nIY8sUHOBaqxPfkaKBg';
 
